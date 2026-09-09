4af57e027dc035b7e9293b33b0801da27fc1ada2
CJ <115610242+Chieri-JN@users.noreply.github.com>
2026-09-09 16:05:46 -0700

    Add: eating-bananas - submission-5

---
+++ Data Structures & Algorithms/eating-bananas/submission-5.py
@@ -0,0 +1,52 @@
+"""
+gievn list of piles where piles[i] is the number of banaas in that pile
+we are also given h, the number of hours we have to eat all the banas. The goal is to find the min
+k such that we can eat all the banaas in h/ 
+
+Constraits: 
+    - must eat all banas in h hours
+    - 
+
+inital ideas / human: 
+    - pick a k that already exists within the pile 
+        - we start with the biggest k and smallest k, if it works we use "bin search" to find
+        - we then cehck that k against the nums 
+    - runtime O(nlogn) [sort] + O(nlogn) logn search * O(n) for check 
+
+    - condition, if tmpH > h, then we need to find a bigger k ,
+    - if tmpH <= h we will try to find a smaller k
+
+"""
+import math
+
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        
+        piles.sort()
+        k = float("inf")
+        lo, hi = 1, piles[-1]
+
+        while lo <= hi:
+            mid = (lo + hi) // 2
+            # tmpK = piles[mid]
+            tmpK = mid
+            tmpH = 0
+            # print(f"lo: {lo}, hi: {hi}, mid: {mid}")
+            # print(f"tmpK: {tmpK}")
+            for p in piles:
+                tmpH += math.ceil(p / tmpK)
+                # print(f"tmpH: {tmpH}")
+                # print(f"--->{math.ceil(p / tmpK)} hours to eat {p} ")
+                if tmpH > h:
+                    break
+            if tmpH > h:
+                lo = mid + 1
+            else: 
+                k = min(k, tmpK)
+                hi = mid 
+                if hi == lo:
+                    break
+
+
+
+        return k
