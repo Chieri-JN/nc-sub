e60e0c27d5a3bbbb4597b3b30790ddb513f05775
CJ <115610242+Chieri-JN@users.noreply.github.com>
2026-09-16 20:05:32 -0700

    Add: longest-repeating-substring-with-replacement - submission-3

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-3.py
@@ -0,0 +1,77 @@
+"""
+want to find longest ss of consec chars with k replacements
+
+insights: 
+    - most frequent char in window is part of the longest ss
+    - once the diff betwen WindowSize - freqCharCount > k we need to shrink window
+
+    - one challenge is how do we find most freqChar in window?, its easy to switch max
+        - but what if when we shrink window we reduce freqChar and another one beats it
+            -  could we just recheck for updated window? is thisx 
+            - can we do range max? this is still logn 
+        - when we switch max we also switch most freq and what eats into k
+            - we remove until condition changes or new char is new max
+                - what if the new max isnt the new char iof k is 
+                - what if we keep 2 maps. char-count, count-char that way when we lose th max we can pick 
+                    n
+AAACBBCBBB
+k=3
+
+s="AAAAABBBBCBB"
+k=3
+
+
+
+"""
+from collections import defaultdict
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        n = len(s)
+        charFreq = {}
+        freqChar = defaultdict(set)
+        mChar = None
+        j = 0
+        maxSS = 0
+        for i in range(n):
+            # if j >= 91:
+                # print(f"CURR SS s[{j}:{i}+1]: {s[j:i+1]}")
+            # print(f"charFreq ---> {charFreq}")
+            # print(f"freqChar ---> {freqChar}")
+            # print(f"mChar: {mChar}")
+            count = charFreq.get(s[i], 0)
+            charFreq[s[i]] = count + 1
+            freqChar[count].remove(s[i]) if s[i] in freqChar[count] else None
+            freqChar[count + 1].add(s[i])
+            # if j >= 90:
+                # print(f" charFreq[{mChar}] > charFreq[{s[i]}] {charFreq[mChar] > charFreq[s[i]]}")
+            mChar = mChar if mChar and charFreq[mChar] > charFreq[s[i]]  else s[i]
+            # print(f"k used with mChar of {mChar}: {(i - j) - charFreq[mChar]}")
+            # print(f"S (mChar): {charFreq[mChar]}, j:{j}, i:{i}")
+            # print(f"{(i - j + 1) - charFreq[mChar]} > {k}")
+            # if j >= 90: 
+                # print(f"charFreq[S] ---> {charFreq.get("S", 0)}")
+                # print(f"S {mChar}: {charFreq[mChar]}, j:{j}, i:{i}")
+                # print(f"{(i - j + 1) - charFreq[mChar]} > {k}")
+            while (i - j + 1) - charFreq[mChar] > k:
+                
+                rmC = s[j]
+                count = charFreq[rmC]
+                # if j >= 90:
+                    # print(f"BEFORE {s[j:i+1]}")
+                    # print(f"charFreq[S] ---> {charFreq.get("S", 0)}")
+                    # print(f"--- rmC: {rmC}, count: {count}")
+                charFreq[rmC] = count - 1
+                freqChar[count].remove(rmC)
+                freqChar[count - 1].add(rmC)
+                if len(freqChar[count]) > 0 and rmC == mChar: 
+                    mChar = freqChar[count].pop()
+                    freqChar[count].add(mChar)
+                j += 1
+                # if j >= 90:
+                    # print(f"AFTER {s[j:i+1]}")
+                    # print(f"mChar {mChar}: {charFreq[mChar]}, j:{j}, i:{i}")
+                    # print(f"charFreq[S] ---> {charFreq.get("S", 0)}")
+            maxSS = max(i - j, maxSS)
+
+        return maxSS +1
+        
